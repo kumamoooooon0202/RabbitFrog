6952eddec9f9a9129a0df123157c4f83507d74b2
KumakuraToyotsugu <tokkun.nga-.0423@ezweb.ne.jp>
2020-12-04 09:26:25 +0900

    おすすめ編成型の完成

--- RabbitFrog/Assets/WorkFolder/LEEGeonWhi/Script/AutoDeckSet.cs
+++ RabbitFrog/Assets/WorkFolder/LEEGeonWhi/Script/AutoDeckSet.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public class AutoDeckArray
+{
+    [Header("自動セットしたいカード")]
+    public CardPoolObject[] autoSet = new CardPoolObject[8];
+}
+
 public class AutoDeckSet : MonoBehaviour
 {
     [SerializeField] private GameObject[] deck = new GameObject[8];
     //[SerializeField] private Image[] deckImage = new Image[8];
     //[SerializeField] private DeckObject[] deckObjects = new DeckObject[8];
-    [SerializeField, Header("自動セットしたいカード")] private CardPoolObject[] cardPoolObjects = new CardPoolObject[8];
+    //[SerializeField, Header("自動セットしたいカード")] private CardPoolObject[] cardPoolObjects = new CardPoolObject[8];
+    [Header("おすすめ編成のパターン")] public AutoDeckArray[] autoDeckArray;
 
-    public void AutoSetDeck()
+    public void AutoSetDeck(int a)
     {
         for(int i = 0; i < deck.Length; i++)
         {
-            deck[i].GetComponent<DeckObject>().cardPoolObject = cardPoolObjects[i];
+            //deck[i].GetComponent<DeckObject>().cardPoolObject = cardPoolObjects[i];
+            deck[i].GetComponent<DeckObject>().cardPoolObject = autoDeckArray[a].autoSet[i];
             //deckObjects[i].cardPoolObject = cardPoolObjects[i];
             deck[i].GetComponent<Image>().sprite = deck[i].GetComponent<DeckObject>().cardPoolObject.character.image;
             //deckImage[i].sprite = deckObjects[i].cardPoolObject.character.image;
